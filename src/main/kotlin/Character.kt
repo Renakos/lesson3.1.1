be66d75205207cf36de328cf311ca3ab56e9e330
Renakos <renakos0129@gmail.com>
2023-11-29 14:52:05 +0600

    Initial commit

--- src/main/kotlin/Character.kt
+++ src/main/kotlin/Character.kt
@@ -1,9 +1,9 @@
-sealed class Character(val name: String, val weaponType: WeaponType) {
+sealed class Character(val name: CustomString, val weaponType: WeaponType) {
 
 
-    class Warrior(name: String, val ability: String) : Character(name, WeaponType.SWORD)
-    class Mage(name: String, val spell: String) : Character(name, WeaponType.STICK)
-    class Archer(name: String, val ability: String) : Character(name, WeaponType.BOW)
+    class Warrior(name: CustomString, val ability: CustomString) : Character(name, WeaponType.SWORD)
+    class Mage(name: CustomString, val spell: CustomString) : Character(name, WeaponType.STICK)
+    class Archer(name: CustomString, val ability: CustomString) : Character(name, WeaponType.BOW)
 
     companion object {
         const val WARRIOR_NAME = "WarriorName"
